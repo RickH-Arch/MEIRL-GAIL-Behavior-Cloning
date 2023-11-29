d8546843aae8ba52dcf8d2c06b6604dea6a3f8f4
RickH-Arch <ricksemail@163.com>
2023-11-29 13:17:50 +0800

    1129 update

--- plot/myplot.py
+++ plot/myplot.py
@@ -85,7 +85,7 @@ def Double_Axes_Line(df,xAxis_str,line1_str,line2_str,xAxes_name = "",line1_name
 def Date_Mac_3D_scatter(df,x_name,y_name,z_name,species_name):
     fig = px.scatter_3d(df, x=x_name, y=y_name, z=z_name,
               color=species_name, 
-              symbol=species_name, opacity=0.7)
+               opacity=0.7)
 
     # tight layout
     fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
@@ -106,5 +106,45 @@ def Surface3D(z_mat,x,y,x_name = "",y_name = ""):
     #     fig.update_yaxes(title_text="<b>%i</b>"%y_name)
     fig.show()
 
+def Surface3D_supPlot(data_tuple_list):
+    if len(data_tuple_list) == 0:
+        return
+    col_num = 3
+    row_num = float.__ceil__(len(data_tuple_list)/col_num)
+    #get specs
+    list_specs = []
+    for i in range(row_num):
+        l = []
+        for j in range(col_num):
+            l.append({'type':'surface'})
+        list_specs.append(l)
+    #get name tuple
+    name_list = []
+    for i in range(len(data_tuple_list)):
+        name_list.append(data_tuple_list[i][3])
+    name_tuple = tuple(name_list)
 
+    #get fig
+    fig = make_subplots(
+    rows=row_num, cols=col_num,
+    specs=list_specs,
+    subplot_titles=name_tuple
+    )
 
+    for i in range(len(data_tuple_list)):
+        t = data_tuple_list[i]
+        row_loc = int(i/col_num)+1
+        col_loc = i%col_num+1
+        fig.add_trace(
+            go.Surface(x=t[1], y=t[2], z=t[0], colorscale='Viridis', showscale=False),
+            row=row_loc, col=col_loc)
+        fig.update_xaxes(title_text = "min_samples",row = row_loc,col=col_loc)
+        fig.update_yaxes(title_text = 'eps',row = row_loc,col=col_loc)
+    
+    fig.update_layout(
+        title_text='Cluster Result',
+        height=400*row_num,
+        width=1000
+    )
+
+    fig.show()
