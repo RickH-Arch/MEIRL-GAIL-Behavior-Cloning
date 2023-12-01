24e9a4bdab862c99d68168ec68684c64d7da6a04
RickH-Arch <ricksemail@163.com>
2023-12-01 17:30:37 +0800

    isolation forest apply

--- utils/utils.py
+++ utils/utils.py
@@ -6,3 +6,11 @@ def Normalize_arr(arr):
     normalized_arr = (arr - min_val) / (max_val - min_val)
     return normalized_arr
     
+def Normalize_df(df_in,cols = [],mul_index = 10):
+    df = df_in.copy()
+    if len(cols) == 0:
+        cols = df.columns.tolist()
+    for col in cols:
+        df[col] = Normalize_arr(df[col])*mul_index
+    
+    return df
