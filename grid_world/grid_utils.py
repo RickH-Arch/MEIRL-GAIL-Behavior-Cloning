e6f8e593bbdc3743c0c37ac3bec0d115d5d6a757
RickH-Arch <ricksemail@163.com>
2023-12-21 22:06:43 +0800

    about to export traj

--- grid_world/grid_utils.py
+++ grid_world/grid_utils.py
@@ -36,3 +36,55 @@ def DrawPathOnGrid(grid,point1, point2):
     
     grid[y1][x1] += 1
     return grid
+
+def GetPathCorList(grid,point1,point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    sx = -1 if x1 > x2 else 1
+    sy = -1 if y1 > y2 else 1
+    err = dx - dy
+    cor_list = []
+    while x1 != x2 or y1 != y2:
+        cor_list.append((x1,y1))
+        e2 = 2 * err
+        movex = False
+        movey = False
+        if e2 > -dy:
+            err -= dy
+            x1 += sx
+            movex = True
+        if e2 < dx:
+            err += dx
+            y1 += sy
+            movey = True
+        if movex and movey:
+            if random.random() <0.5:
+                cor_list.append((x1-sx,y1))
+            else:
+                cor_list.append((x1,y1-sy))
+    
+    cor_list.append((x1,y1))
+    return cor_list
+
+def StatesToStateActionPairs(states):
+    '''
+    actions: 0:stay,1:up,2:down,3:left,4:right
+    '''
+    pairs = []
+    for i in range(len(states)-1):
+        if states[i] == states[i+1]:
+            pairs.append([states[i],0])
+        elif states[i][0] == states[i+1][0]:
+            if states[i][1] == states[i+1][1] + 1:
+                pairs.append([states[i],2])
+            elif states[i][1] == states[i+1][1] - 1:
+                pairs.append([states[i],1])
+        elif states[i][1] == states[i+1][1]:
+            if states[i][0] == states[i+1][0] + 1:
+                pairs.append([states[i],3])
+            elif states[i][0] == states[i+1][0] - 1:
+                pairs.append([states[i],4])
+        
+    return pairs
--- grid_world/grid_world.py
+++ grid_world/grid_world.py
@@ -5,10 +5,17 @@ from utils import utils
 from grid_world import grid_utils,grid_plot
 import math
 from datetime import datetime
+import pickle
 
 
 class GridWorld:
+    '''
+    class to initialize grid world, record active state, convert path to state action pairs, parce enviroment factors, etc.
+    actions: 0:stay,1:up,2:down,3:left,4:right
+    '''
     def __init__(self, df_wifipos,df_path,width = 100,height = 75) -> None:
+        self.width = width
+        self.height = height
         self.empty_grid = np.zeros((height,width))
         self.count_grid = np.zeros((height,width))
         self.freq_grid = np.zeros((height,width))
@@ -29,6 +36,33 @@ class GridWorld:
                 self.count_grid= grid_utils.DrawPathOnGrid(self.count_grid,point1,point2)
         
         self.SavePathCount(f'wifi_track_data/dacang/grid_data/count_grid_{self.date}.npy')
+
+    def PathToStateActionPairs(self,df,scale = 1):
+        mac_list = df.m.unique()
+        state_list = []
+        for m in tqdm(mac_list):
+            state = []
+            df_now = utils.GetDfNow(df,m)
+            x,y,z = utils.GetPathPointsWithUniformDivide(df_now,self.df_wifipos,self.df_path)
+            for i in range(len(x)-1):
+                point1 = (math.floor(x[i]*scale),math.floor(y[i]*scale))
+                point2 = (math.floor(x[i+1]*scale),math.floor(y[i+1]*scale))
+                state.extend(grid_utils.GetPathCorList(self.count_grid,point1,point2))
+            state_list.append(state)
+        print("Converting to state action pairs...")
+        pairs_list = []
+        for i in range(len(state_list)):
+            states = state_list[i]
+            pairs = grid_utils.StatesToStateActionPairs(states)
+            for pair in pairs:
+                pair[0] = self.CoordToState(pair[0])
+            pairs_list.append(pairs)
+        pairs_dict = dict(zip(mac_list, pairs_list))
+        df = pd.DataFrame({"m":mac_list,'trajs':pairs_list})
+        df.to_csv(f'wifi_track_data/dacang/track_data/trajs_{self.date}.csv',index=False)
+        return df
+
+        
         
     def ReadPathCount(self,path):
         self.count_grid = np.load(path)
@@ -68,9 +102,6 @@ class GridWorld:
                 states.append(self.CoordToState([j,i]))
         return states
     
-    def GetStateActionPairs(self,df_now):
-        x,y,z = utils.GetPathPointsWithUniformDivide(df_now,self.df_wifipos,self.df_path)
-
     def ShowGridWorld_Count(self):
         grid_plot.ShowGridWorld(self.count_grid)
 
