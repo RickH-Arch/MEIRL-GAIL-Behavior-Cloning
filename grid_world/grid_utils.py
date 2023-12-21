eaa6e36c9fb187f0a9346e63fdf78bb1a11c04db
RickH-Arch <ricksemail@163.com>
2023-12-21 15:57:07 +0800

    grid world start

--- grid_world/grid_utils.py
+++ grid_world/grid_utils.py
@@ -3,5 +3,24 @@ import numpy as np
 from utils import utils
 
 
-def TrackToGridPoints(df_now,df_wifipos,df_path):
-    x,y,z = utils.GetPathPoints(df_now,df_wifipos,df_path)
+def DrawPathOnGrid(grid,point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    sx = -1 if x1 > x2 else 1
+    sy = -1 if y1 > y2 else 1
+    err = dx - dy
+
+    while x1 != x2 or y1 != y2:
+        grid[y1][x1] += 1
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x1 += sx
+        if e2 < dx:
+            err += dx
+            y1 += sy
+    
+    grid[y1][x1] += 1
+    return grid
