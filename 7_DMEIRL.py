e7af6ab3f691822a1b5ce6086b5fb11065785be8
RickH-Arch <ricksemail@163.com>
2024-01-25 19:31:56 +0800

    0125 train

--- 7_DMEIRL.py
+++ 7_DMEIRL.py
@@ -4,21 +4,21 @@ from DMEIRL.DeepMEIRL_FC import DMEIRL
 
 #------------------------------------Initialize Grid World------------------------------------------
 
-env_folder_path = "wifi_track_data/dacanggrid_data/envs_grid/1230"
-feature_folder_path = "wifi_track_data/dacang/grid_data/features_grid/1230_80x60"
-expert_traj_path = "wifi_track_data/dacang/track_data/trajs_1230.csv"
+env_folder_path = r"wifi_track_data\dacang\grid_data\envs_grid\0117_40x30"
+feature_folder_path = r"wifi_track_data\dacang\grid_data\features_grid\0117_40x30"
+expert_traj_path = r"wifi_track_data\dacang\track_data\trajs_0117_40x30.csv"
 
 world = GridWorld(features_folderPath=feature_folder_path,
                   expert_traj_filePath=expert_traj_path,
-                  width=80, height=60)
+                  width=40, height=30)
 df_cluster = pd.read_csv('wifi_track_data/dacang/cluster_data/cluster_result_0105.csv')
-world.experts.ReadCluster(df_cluster)
-world.experts.ApplyCluster((0,1,2))
+#world.experts.ReadCluster(df_cluster)
+#world.experts.ApplyCluster((0,1,2))
 print("GridWorld initialized")
 
 #------------------------------------Initialize DMEIRL------------------------------------------
 
-dme = DMEIRL(world,layers=(60,30,30),lr=0.0001,clip_norm=0.1,weight_decay=0.1,log='0115')
+dme = DMEIRL(world,layers=(30,60,30),lr=0.0005,weight_decay=0.5,log='0123',log_dir='run')
 
 #------------------------------------Train------------------------------------------
 
